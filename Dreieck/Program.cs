abb3154c7d45e144f9b7d402ef0e9e4527745820
nilsandresen <andresennils68@gmail.com>
2020-09-19 18:48:07 +0200

    Projektdateien hinzufügen.

---
+++ Dreieck/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Dreieck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RechtwinkligesDreieck dreieck_1 = new RechtwinkligesDreieck();
+            string text;
+            double zahl;
+            bool ok = true;
+            do
+            {
+                Console.WriteLine("Geben Sie den Winkel alpha ein.");
+                text = Console.ReadLine();
+                ok = Double.TryParse(text, out zahl);
+                if (ok)
+                {
+                    dreieck_1.SetAlpha(zahl);
+                }
+            } while (!ok);
+            Console.WriteLine("Geben Sie die länge der Hypothenuse ein.");
+            dreieck_1.SetHypothenuse(Double.Parse(Console.ReadLine()));
+            // Console.WriteLine(dreieck_1.GetAlpha());
+            Console.WriteLine("Beta ist " + dreieck_1.GetBeta()+"°");
+            Console.WriteLine("Die Gegenkathete von Alpha ist " + Math.Round(dreieck_1.GetKathete_a(),2) + " Längeneinheiten");
+        }
+    }
+}
---
+++ Dreieck/RechtwinkligesDreieck.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dreieck
+{
+    class RechtwinkligesDreieck
+    {
+        private double alpha;
+        private double hypothenuse;
+        private double beta;
+        private double kathete_a;
+        private double kathete_b;
+        public double GetAlpha()
+        {
+            return alpha;
+        }
+        public double GetBeta()
+        {
+            return beta;
+        }
+
+        public double GetKathete_a()
+        {
+            kathete_a = Math.Sin(alpha / 180 * Math.PI) * hypothenuse;
+            return kathete_a;
+        }
+        public double GetKathete_b()
+        {
+            kathete_b = Math.Sin(beta / 180 * Math.PI) * hypothenuse;
+            return kathete_b;
+        }
+
+        public double GetHypothenuse()
+        {
+            return hypothenuse;
+        }
+
+        public void SetAlpha(double alpha)
+        {
+            if (alpha > 0 && alpha < 90)
+            {
+                this.alpha = alpha;
+                beta = 90 - alpha;
+            }
+        }
+        
+        public void SetHypothenuse(double hypothenuse)
+        {
+            this.hypothenuse = hypothenuse;
+        }
+
+
+    }
+}
